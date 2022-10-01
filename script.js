6c42d1e52acb137cedbfcdadc05cd68404bd3b22
nickbloo <nickbloo2@gmail.com>
2022-10-01 17:37:38 -0500

    new commit

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log("Test message");
+
+let startingNum = 0;
+let timeNum= 0;
+let dataNum = 0;
+
+const gameState = {
+    totalNumOfClicks: 0,
+    numOfPassiveClicks: 0,
+    intervalLoopID: undefined
+};
+
+let scoreCount = document.getElementById("score-count");
+let timeCount = document.getElementById("time-count");
+let dataCount = document.getElementById("data-count");
+let clickImg = document.getElementById("click-img");
+
+setInterval(function() {
+    timeNum ++;
+    timeCount.textContent = timeNum + "s";
+}, 1000);
+
+setInterval(function() {
+    dataNum += 14;
+    dataCount.textContent = dataNum;
+}, 222);
+
+function clickIncrementer() {
+    startingNum ++;
+    scoreCount.textContent = startingNum;
+};
+
+clickImg.addEventListener("click", clickIncrementer);
+
+if (scoreCount.textContent == 10) {
+    document.getElementsById("prod1").removeAttribute("hidden")
+};
