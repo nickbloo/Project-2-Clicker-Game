6c47e9c4e8847cdea2a2005dbd3e3b445e447ad2
nickbloo <nickbloo2@gmail.com>
2022-10-01 19:11:45 -0500

    added unhidden elements

--- script.js
+++ script.js
@@ -28,10 +28,31 @@ setInterval(function() {
 function clickIncrementer() {
     startingNum ++;
     scoreCount.textContent = startingNum;
+
 };
 
 clickImg.addEventListener("click", clickIncrementer);
 
-if (scoreCount.textContent == 10) {
-    document.getElementsById("prod1").removeAttribute("hidden")
-};
+setInterval(function() {
+    if (startingNum >= 10) {
+        document.getElementById("prod1").removeAttribute("hidden");
+    } if (startingNum >= 40) {
+        document.getElementById("prod2").removeAttribute("hidden");
+    } if (startingNum >= 200) {
+        document.getElementById("prod3").removeAttribute("hidden");
+    } if (startingNum >= 900) {
+        document.getElementById("prod4").removeAttribute("hidden");
+    } if (startingNum >= 9500) {
+        document.getElementById("prod5").removeAttribute("hidden");
+    } if (startingNum >= 95000) {
+        document.getElementById("prod6").removeAttribute("hidden");
+    } if (startingNum >= 999999) {
+        document.getElementById("prod7").removeAttribute("hidden");
+    }
+}, 100);
+
+
+
+// Step 2 - Clicking buy button adds a producer to inventory
+//      Step 2a - each producer adds a number per second to the scoreCount
+//      Step 2b - producers do not stop producing (while loop?)
