36df243bd46932a4633d8fdfcab6c6567896c088
nickbloo <nickbloo2@gmail.com>
2022-10-02 20:05:20 -0500

    final update

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-console.log("Test message");
+let startingScore = 0; // Defines the starting score
+let timeNum= 0; // Defines the starting number on the timer
+let dataNum = 0; // Defines the starting number on data counter
 
-let startingNum = 0;
-let timeNum= 0;
-let dataNum = 0;
-
+// Sets the html button elements to js variables
 let buttonOne = document.getElementById("button1");
 let buttonTwo = document.getElementById("button2");
 let buttonThree = document.getElementById("button3");
@@ -12,6 +11,7 @@ let buttonFive = document.getElementById("button5");
 let buttonSix = document.getElementById("button6");
 let buttonSeven = document.getElementById("button7");
 
+// Sets the html element that counts each number of producers owned to a js variable
 let itemCountOne = document.getElementById("producer-count-1");
 let itemCountTwo = document.getElementById("producer-count-2");
 let itemCountThree = document.getElementById("producer-count-3");
@@ -19,7 +19,8 @@ let itemCountFour = document.getElementById("producer-count-4");
 let itemCountFive = document.getElementById("producer-count-5");
 let itemCountSix = document.getElementById("producer-count-6");
 let itemCountSeven = document.getElementById("producer-count-7");
-    
+
+// Creates an object setting each button variable to a value equal to the amount of clicks they generate per second    
 const producerButtonValues = {
     buttonOne: 1,
     buttonTwo: 5,
@@ -30,125 +31,142 @@ const producerButtonValues = {
     buttonSeven: 1000000
 };
 
+// Sets html elements of the score count, timer, data count, image that gets clicked on and the passive producer count to js variables
 let scoreCount = document.getElementById("score-count");
 let timeCount = document.getElementById("time-count");
 let dataCount = document.getElementById("data-count");
 let clickImg = document.getElementById("click-img");
 let passiveCountTotal = document.getElementById("passive-count");
 
+// Creates a timer by increasing the timeNum variable by 1 every second and setting the timeCount html text to the variable
 setInterval(function() {
     timeNum ++;
     timeCount.textContent = timeNum + "s";
 }, 1000);
 
+// Creates a number generator that increases the "data count" by 14 every 222 milliseconds
 setInterval(function() {
     dataNum += 14;
     dataCount.textContent = dataNum;
 }, 222);
 
+// Creates a function that increases the startingScore variable by 1 and sets the score count html text to the variable
 function clickIncrementer() {
-    startingNum ++;
-    scoreCount.textContent = startingNum;
+    startingScore ++;
+    scoreCount.textContent = startingScore;
 };
 
-clickImg.addEventListener("click", clickIncrementer);
 
+clickImg.addEventListener("click", clickIncrementer); // Event listener that invokes the clickIncrementor function every time the clickImg is clicked
+
+// A function set every 100 milliseconds that checks if the startingScore is equal to certain numbers
+// When the score is equal to or greater than these numbers the hidden attribute is removed from the producer elements
+// Making them available for purchase
 setInterval(function() {
-    if (startingNum >= 10) {
+    if (startingScore >= 10) {
         document.getElementById("prod1").removeAttribute("hidden");
-    } if (startingNum >= 40) {
+    } if (startingScore >= 40) {
         document.getElementById("prod2").removeAttribute("hidden");
-    } if (startingNum >= 200) {
+    } if (startingScore >= 200) {
         document.getElementById("prod3").removeAttribute("hidden");
-    } if (startingNum >= 900) {
+    } if (startingScore >= 900) {
         document.getElementById("prod4").removeAttribute("hidden");
-    } if (startingNum >= 9500) {
+    } if (startingScore >= 9500) {
         document.getElementById("prod5").removeAttribute("hidden");
-    } if (startingNum >= 95000) {
+    } if (startingScore >= 95000) {
         document.getElementById("prod6").removeAttribute("hidden");
-    } if (startingNum >= 999999) {
+    } if (startingScore >= 999999) {
         document.getElementById("prod7").removeAttribute("hidden");
     }
 }, 100);
 
+// All the following functions created that checks if the score is greater than or equal to 10
+// If it is, then ten is subtracted from the score
+// A newNum variable is created equal to the html text of itemCountOne
+// newNum is increased by one and the itemCountOne is set to the updated value
 function passiveProductionOne() {
-    if (startingNum >= 10) {
-        startingNum -= 10;
+    if (startingScore >= 10) {
+        startingScore -= 10;
         let newNum = itemCountOne.innerText;
         newNum++
         itemCountOne.innerText = newNum
     }
 };
 
-    buttonOne.addEventListener("click", passiveProductionOne);
+// Creates on click event for buttonOne that invokes the above function
+// All the following on click functions do the same for their respective buttons and functions
+buttonOne.addEventListener("click", passiveProductionOne);
 
 function passiveProductionTwo() {
-    if (startingNum >= 50) {
-        startingNum -= 50;
+    if (startingScore >= 50) {
+        startingScore -= 50;
         let newNum = itemCountTwo.innerText;
         newNum++
         itemCountTwo.innerText = newNum
     }
 };
 
-    buttonTwo.addEventListener("click", passiveProductionTwo);
+buttonTwo.addEventListener("click", passiveProductionTwo);
 
 function passiveProductionThree() {
-    if (startingNum >= 250) {
-        startingNum -= 250;
+    if (startingScore >= 250) {
+        startingScore -= 250;
         let newNum = itemCountThree.innerText;
         newNum++
         itemCountThree.innerText = newNum
     }
 };
 
-    buttonThree.addEventListener("click", passiveProductionThree);
+buttonThree.addEventListener("click", passiveProductionThree);
 
 function passiveProductionFour() {
-    if (startingNum >= 1000) {
-        startingNum -= 1000;
+    if (startingScore >= 1000) {
+        startingScore -= 1000;
         let newNum = itemCountFour.innerText;
         newNum++
         itemCountFour.innerText = newNum
     }
 };
 
-    buttonFour.addEventListener("click", passiveProductionFour);
+buttonFour.addEventListener("click", passiveProductionFour);
 
 function passiveProductionFive() {
-        if (startingNum >= 10000) {
-            startingNum -= 10000;
+        if (startingScore >= 10000) {
+            startingScore -= 10000;
             let newNum = itemCountFive.innerText;
             newNum++
             itemCountFive.innerText = newNum
         }
     };
     
-    buttonFive.addEventListener("click", passiveProductionFive);
+buttonFive.addEventListener("click", passiveProductionFive);
 
 function passiveProductionSix() {
-        if (startingNum >= 100000) {
-            startingNum -= 100000;
+        if (startingScore >= 100000) {
+            startingScore -= 100000;
             let newNum = itemCountSix.innerText;
             newNum++
             itemCountSix.innerText = newNum
         }
     };
     
-    buttonSix.addEventListener("click", passiveProductionSix);
+buttonSix.addEventListener("click", passiveProductionSix);
 
 function passiveProductionSeven() {
-        if (startingNum >= 1000000) {
-            startingNum -= 1000000;
+        if (startingScore >= 1000000) {
+            startingScore -= 1000000;
             let newNum = itemCountSeven.innerText;
             newNum++
             itemCountSeven.innerText = newNum
         }
     };
     
-    buttonSeven.addEventListener("click", passiveProductionSeven);
+buttonSeven.addEventListener("click", passiveProductionSeven);
 
 
+// A function that sets new varables sum(s) 1-7 to the product of this equation:
+// itemCount html text(converted to a Number) * the value of the corresponding producerButtonValue keys
+// Then these sums are added together and returned for a total value of what all current producers are producing
 function multiplyPassiveSums() {
     let sumOne = Number(itemCountOne.innerText) * producerButtonValues.buttonOne;
     let sumTwo = Number(itemCountTwo.innerText) * producerButtonValues.buttonTwo;
@@ -160,11 +178,15 @@ function multiplyPassiveSums() {
         return sumOne + sumTwo + sumThree + sumFour + sumFive + sumSix + sumSeven
 };
 
+// Function that sets the passiveCountTotal html text to the above sum and updates this value every 100ms
 setInterval(function() {
     passiveCountTotal.innerText = multiplyPassiveSums()
 }, 100);
 
+// Function that adds the sum of all the passive production (the multiplyPassiveSums function) to the starting score 
+// Then the scoreCount html text is set as equal to that final sum
+// This is run once every second
 setInterval(function() {
-    startingNum += multiplyPassiveSums();
-    scoreCount.innerText = startingNum
+    startingScore += multiplyPassiveSums();
+    scoreCount.innerText = startingScore
 }, 1000);
